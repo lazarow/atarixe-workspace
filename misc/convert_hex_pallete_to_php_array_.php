f3898bf551fe7a2988e2e31dd43d238a09f5d942
Arkadiusz Nowakowski <nowakowski.arkadiusz@gmail.com>
2020-06-16 12:29:03 +0200

    feat: add atari colors palette

---
+++ misc/convert_hex_pallete_to_php_array_.php
@@ -0,0 +1,23 @@
+<?php
+$colors = file(__DIR__ . '/paleta_kolorow_w_hex.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+$hue = 0;
+$luminance = 0;
+$colorsData = [];
+foreach ($colors as $hex) {
+    list($r, $g, $b) = sscanf($hex, "%02x%02x%02x");
+    $colorData = '[' . implode(', ', [
+        "'$hex'", $r, $g, $b, $hue * 16 + $luminance * 2, "'" . str_pad(dechex($hue * 16 + $luminance * 2), 2, '0', STR_PAD_LEFT) . "'"
+    ]) . ']';
+    $colorsData[] = $colorData;
+    echo $colorData . PHP_EOL;
+    $luminance++;
+    if ($luminance > 7) {
+        $hue++;
+        $luminance = 0;
+    }
+}
+echo 'PHP ARRAY STARTS HERE:' . PHP_EOL;
+echo '[' . PHP_EOL . implode(',' . PHP_EOL, array_map(function ($colorData) {
+    return "\t" . $colorData;
+}, $colorsData)) . PHP_EOL . ']';
